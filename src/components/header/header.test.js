9c4332eda6649b7e4871b599cffd87a8ea365109
Himanshu Uppal <himanshu.uppal@nagarro.com>
2020-05-27 00:17:05 +0530

    tried unit testing

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,2 +1,13 @@
+import React from 'react';
 import {Header} from './header';
 import {mount} from 'enzyme';
+
+ 
+describe('testing Header', () => {
+    let header = mount (<Header />);    
+
+    it('renders the button tag for logout' , () =>{
+        expect(header.find('button').text().toEqual('Logout'))
+    })
+})
+
